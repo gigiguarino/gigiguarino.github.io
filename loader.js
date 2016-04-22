e318fc147997866942436189d0915e44a4996a6f
Gabrielle Guarino <gguarino@umich.edu>
2016-04-22 15:54:14 -0400

    finally
    fades in, and dynamically loads

--- loader.js
+++ loader.js
@@ -1,9 +1,17 @@
-$(document).ready(function(){
-	$('#content').load('about.html');
-});
+var page;
 
-$('div#links a').click(function(){
-	var page = $(this).attr('href');
-	$('#content').load(page + '.html');
-	return false;
+$('a').click(function(){
+	if ($(this).attr("class") == "invalid")
+	{
+		$("#invisible").css("display", "none");
+		page = $(this).attr("href");
+		$("#content").load(page + ".html", function(){
+			$("#invisible").fadeIn("slow");
+		});
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 });
