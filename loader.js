8191b954a9b53db9dfe6ce3059e330c867d996b2
Gabrielle Guarino <gguarino@umich.edu>
2016-04-21 20:33:52 -0400

    fixed dynamic stuff
    the page was redirecting automatically

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('#content').load('email.html');
 	$('div#links a').click(function(){
 		var page = $(this).attr('href');
-		$('#content').load(page);
+		$('#content').load(page + '.html');
 		return false;
 	});
 });
