cf3298ee37b5c36998aa92bc6df38f4b36a0a30a
Gabrielle Guarino <gguarino@umich.edu>
2016-04-21 20:27:50 -0400

    dynamic
    trying to dynamically load pages

---
+++ loader.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('#content').load('email.html');
+	$('div#links a').click(function(){
+		var page = $(this).attr('href');
+		$('#content').load(page);
+		return false;
+	});
+});
