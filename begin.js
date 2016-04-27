4ea3de9407824ee53f65ffeb94c8842ac95ae442
Gabrielle Guarino <gguarino@umich.edu>
2016-04-27 12:46:57 -0400

    image
    added a cute gif and some layout changes

--- begin.js
+++ begin.js
@@ -1,9 +1,5 @@
 
 
 $(document).ready(function(){
-	$("#content").load(page + ".html", function(){
-			$("#invisible").fadeIn("fast", function(){
-				$("#content").fadeIn("slow");
-			});
-		});
+
 })
