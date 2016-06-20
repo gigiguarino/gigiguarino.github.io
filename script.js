cf86b47c864f6a0ec3bf7674bcb5fcb6c1113642
Gabrielle Guarino <gguarino@umich.edu>
2016-06-20 00:55:56 -0400

    NEW AND FRESH
    completely new design!!! so much better

---
+++ script.js
@@ -0,0 +1,35 @@
+var app = angular.module('website', ['ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/about', {
+			templateUrl: 'about.html'
+		})
+		.when('/classes', {
+			templateUrl: 'classes.html'
+		})
+		.when('/games', {
+			templateUrl: 'games.html'
+		})
+		.when('/contact', {
+			templateUrl: 'contact.html'
+		})
+		.when('/artwork', {
+			templateUrl: 'artwork.html'
+		})
+		.when('/mac-help', {
+			templateUrl: 'mac-help.html'
+		})
+		.when('/windows-help', {
+			templateUrl: 'windows-help.html'
+		})
+		.otherwise( {
+			redirectTo: '/about'
+		});
+}])
+app.controller('mainController', ['$scope', function($scope){
+
+}]);
+app.controller('photoController', ['$scope', function($scope){
+
+}]);
+
